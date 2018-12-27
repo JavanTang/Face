3314fa43c8304961005900c2b1c479525bc8523c
hanbing <beatmight@gmail.com>
2018-12-27 13:43:53 +0800

    增加了tag字段

--- processes/message.py
+++ processes/message.py
@@ -3,11 +3,13 @@ Define some data structure for muti-thread comunication.
 """
 import time
 
+
 class CameraMessage(object):
     """Wrap message transfer from CameraReader to Recognizer
     """
 
-    def __init__(self, image, channel_id):
+    def __init__(self, image, channel_id, tag):
         self.image = image
         self.channel_id = channel_id
         self.record_time = time.time()
+        self.tag = tag
