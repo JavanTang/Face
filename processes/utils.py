6c2fc27ac26855f8fd837a5d3779120bdfcb4eeb
hanbing <beatmight@gmail.com>
2018-12-28 18:20:49 +0800

    增加了Debug的帮助函数

--- processes/utils.py
+++ processes/utils.py
@@ -1,3 +1,6 @@
+import traceback
+
+
 def UnitTestDecorator(obj):
     """单元测试装饰器，在单元测试的时候可以开启单元测试模式，不影响程序正常运行时的状态
     """
@@ -12,3 +15,20 @@ def UnitTestDecorator(obj):
     obj.set_test_option_on = set_test_option_on
     obj.get_test_option = get_test_option
     return obj
+
+
+def DebugPrintStackTrace(func):
+    """多进程运行时出错用于打印错误信息
+    """
+
+    def wrapper(*args, **kwargs):
+
+        try:
+            ret = func(*args, **kwargs)
+        except Exception:
+            msg = traceback.format_exc()
+            print(msg)
+
+        return ret
+
+    return wrapper
