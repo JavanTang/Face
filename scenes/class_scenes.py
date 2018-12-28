1f8d7e67236955df42f30eaf3baf721d30d2987f
hanbing <beatmight@gmail.com>
2018-12-28 18:22:41 +0800

    将场景移动到项目根目录

--- scenes/class_scenes.py
+++ scenes/class_scenes.py
@@ -2,18 +2,21 @@ import unittest
 import os
 import sys
 import numpy as np
-import insightface
-import db
 import multiprocessing
 import time
-
-from . import recognizer, recorder
-from .queues import ProcessingQueue, RedisQueue
 
 here = os.path.abspath(os.path.dirname(__file__))
 thismodule = sys.modules[__name__]
 
+source_root = os.path.join(here, '..')
+sys.path.append(source_root)
+import algorithm.insightface as insightface
 
+from processes import recognizer, recorder, queues
+from processes.utils import DebugPrintStackTrace
+
+
+@DebugPrintStackTrace
 def start_camera(recorder_name,  # recorder类型
                  camera_id_list,  # 摄像头id列表
                  camera_ip_list,   # 摄像头ip列表
@@ -21,7 +24,8 @@ def start_camera(recorder_name,  # recorder类型
                  tag,  # 摄像头标识
                  queue  # 从摄像头读取图片的存储队列
                  ):
-
+    
+    print("Starting Camera.")
     tasks = []
 
     for i, ip in zip(camera_id_list, camera_ip_list):
@@ -34,6 +38,7 @@ def start_camera(recorder_name,  # recorder类型
         t.join()
 
 
+@DebugPrintStackTrace
 def start_recognizer(engine_name,  # 人脸识别引擎名称
                      face_database_path,  # 人脸库路径
                      minsize,  # 最小人脸像素
@@ -45,12 +50,13 @@ def start_recognizer(engine_name,  # 人脸识别引擎名称
                      monitor_on,  # 是否开启监控模块
                      gpu_id):  # 使用那一块gpu
 
+    print("Starting Recognizer.")
    # 获取人脸识别引擎对象
     engine = getattr(insightface, engine_name)(gpu_id=gpu_id)
     engine.load_database(face_database_path)
 
     if monitor_on:
-        monitor_queue = db.Queue.get_by_name("RedisQueue", "monitor")
+        monitor_queue = queues.Queue.get_by_name("RedisQueue", "monitor")
     else:
         monitor_queue = None
 
@@ -86,15 +92,16 @@ class ScenesClassRoom(object):
         'read_fps_interval': 40,
     }
 
-    def __init__(self, config):
-        self.config = self.default_config.copy().update(config)
+    def __init__(self, config={}):
+        self.config = self.default_config.copy()
+        self.config.update(config)
 
         # 准备进程池以及通讯队列
-        self.pool = multiprocessing.Pool(self.config.p_algorithm_num + 1)
-        self.camrea_queue = db.Queue.get_by_name(
-            "ProcessingQueue", self.config.tag + '_' + 'camera')
-        self.data_queue = db.Queue.get_by_name(
-            "ProcessingQueue", self.config.tag + '_' + "data")
+        self.pool = multiprocessing.Pool(self.config['p_algorithm_num'] + 1)
+        self.camrea_queue = queues.Queue.get_by_name(
+            "ProcessingQueue", self.config['tag'] + '_' + 'camera')
+        self.data_queue = queues.Queue.get_by_name(
+            "ProcessingQueue", self.config['tag'] + '_' + "data")
 
     def start(self):
         """
@@ -103,7 +110,7 @@ class ScenesClassRoom(object):
 
         # 启动算法模块
         gpu_ids = self.config['gpu_id']
-        for i in range(self.config.p_algorithm_num):
+        for i in range(self.config['p_algorithm_num']):
             gpu_id = gpu_ids[i % len(gpu_ids)]
             self.pool.apply_async(start_recognizer, args=(
                 self.config['engine_name'],
