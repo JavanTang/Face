e1e8c8f8f9d53a5f0a248cf80709f4687c87879a
hanbing <beatmight@gmail.com>
2019-01-02 10:52:55 +0800

    增加了 reader 的测试用例

--- tests/test_node.py
+++ tests/test_node.py
@@ -8,5 +8,22 @@ here = os.path.abspath(os.path.dirname(__file__))
 source_path = os.path.join(here, '../')
 sys.path.append(source_path)
 
-from processes.nodes import recognizer
+from processes.nodes import recorder
 
+
+class TestNode(unittest.TestCase):
+
+    def test_reader(self):
+        reader = recorder.CameraReader()
+        reader.init_algorithm([os.path.join(source_path, 'database/cache/video_friday/4.avi')], [1], 200, 'test')
+        reader.set_test_option_on()
+        reader.run()
+
+        for _ in range(5):
+            ret = reader.get()
+            print(ret)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
