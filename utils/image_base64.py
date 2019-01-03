dfe75efde8692220eb8fac5d48cd1e9685243a1a
唐志峰 <tzfjobmail@gmail.com>
2019-01-03 18:28:19 +0800

    加了一个数组转图片文件的方法

--- utils/image_base64.py
+++ utils/image_base64.py
@@ -1,8 +1,47 @@
 import base64
 import traceback
+import BytesIO
+import numpy as np
+import os
+from .time_utils import genera_stamp
+
+current = os.path.dirname(__name__)
+project = os.path.dirname(current)
+img_path = os.path.join(project, 'database/detect_image') # 图片需要存放的位置
+
+
+def array_to_file(array_data, names):
+    '''数组转图片文件
+
+    Arguments:
+        array_data {array} -- 人脸的数组
+        names {str} -- 人员的编号
+    '''
+    path = [os.path.join(img_path, genera_stamp()) for i in names]
+    for index in len(array_data):
+        img_array = array_data[index]
+        b64_code = array_to_base64(img_array)
+        image = base64_to_image(b64_code)
+        with open(path, 'wb+') as f:
+            f.write(image)
+    return path
+
+
+def array_to_base64(array_data):
+    '''数组转base64
+
+    Arguments:
+        array_data {numpy} -- 图片数组
+    '''
+    arr = np.arange(12).reshape(3, 4)
+    bytesio = BytesIO()
+    np.savetxt(bytesio, arr)  # 只支持1维或者2维数组，numpy数组转化成字节流
+    content = bytesio.getvalue()  # 获取string字符串表示
+    b64_code = base64.b64encode(content)
+    return b64_code
+
 
 def base64_to_image(base64_data):
-
     """
     :param base64_data: 图片的base64编码
     :return: image: 图片对象
@@ -14,8 +53,8 @@ def base64_to_image(base64_data):
     except:
         traceback.print_exc()
 
-def image_to_base64(image):
 
+def image_to_base64(image):
     """
     :param image: 图片所在路径
     :return: base64_data: 图片对应的base64编码
