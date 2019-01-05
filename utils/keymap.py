0e412be0b3846aa561cc8dfc415415fa0c69c11d
唐志峰 <tzfjobmail@gmail.com>
2019-01-05 17:13:32 +0800

    添加ip对应id的映射关系

---
+++ utils/keymap.py
@@ -0,0 +1,14 @@
+'''
+@Author: TangZhiFeng
+@Data: 2019-01-05
+@LastEditors: TangZhiFeng
+@LastEditTime: 2019-01-05 16:03:57
+@Description: 一些对应的映射关系  
+'''
+
+camera_ip2camera_id = {
+    'rtsp://admin:sdkj123456@192.168.0.64:554/Streaming/Channels/101' : '1',
+    'rtsp://admin:admin12345@192.168.0.141:554/Streaming/Channels/101' : '2',
+    'rtsp://admin:admin12345@192.168.0.52:554/Streaming/Channels/101' : '3',
+    'rtsp://admin:admin12345@192.168.0.51:554/Streaming/Channels/101' : '4',
+}
