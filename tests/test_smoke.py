6ea237e85406eeff9bfce59d7b965cfe583f1aa4
hanbing <beatmight@gmail.com>
2019-01-07 10:47:15 +0800

    火焰检测测试通过

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,34 +1,35 @@
+import os
+import sys
 import cv2
+import unittest
 
+here = os.path.abspath(os.path.dirname(__file__))
+
+# 将所要测试的源码路径放入path下面
+source_path = os.path.join(here, '../')
+sys.path.append(source_path)
 from algorithm.fire_detection.smoke_detect import detect, get_default_model
 
 
-class TestFireDetection(object):
+class TestFireDetection(unittest.TestCase):
 
-    @staticmethod
-    def test_smoke_detection():
-
-        print('9999')
+    def test_smoke_detection(self):
 
         sess, model = get_default_model()
 
-        print('3333')
-        cap = cv2.VideoCapture('../algorithm/fire_detection/fire.mp4')
+        cap = cv2.VideoCapture(os.path.join(
+            source_path, 'database/cache/fire.mp4'))
 
         while True:
             res, frame = cap.read()
 
             if not res:
-                break
-            print(res)
+                continue
             # frame = os.path.join(here, '../database/cache/smoke.png')
             # frame = cv2.imread(frame)
 
             flag, frame, cameraKey, image_id = detect(
                 sess, model, frame, frame.shape[0], frame.shape[1], 10, '1')
 
-            print(flag)
-
-
-if __name__ == '__main__':
-    TestFireDetection.test_smoke_detection()
+            assert not flag
+            break
