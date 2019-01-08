72c21231f3f26587c079d851bcf19197e6f6686f
唐志峰 <tzfjobmail@gmail.com>
2019-01-08 16:36:15 +0800

    大门的异常检测

--- scenes/abnormal/school_gate.py
+++ scenes/abnormal/school_gate.py
@@ -2,7 +2,7 @@
 @Author: TangZhiFeng
 @Data: 2019-01-04 
 @LastEditors: TangZhiFeng
-@LastEditTime: 2019-01-05 16:34:56
+@LastEditTime: 2019-01-08 16:35:42
 @Description: 大门异常检测
 '''
 
@@ -14,16 +14,17 @@ current = os.path.dirname(__name__)
 project = os.path.dirname(os.path.dirname(current))
 sys.path.append(project)
 
-
-from utils.image_base64 import array_to_file
-from processes.nodes.diff_node import FrameDiffNode
-from processes.nodes.recorder import CameraReader
-from scenes import BaseEngineering, BaseScenesManage
-from processes.nodes.recognizer import RealTimeRecognizer
-from utils.time_utils import genera_stamp
-from utils.socket_client import client
-from utils.upload_image import batch_people_upload,batch_type2_upload
+from utils.upload_image import batch_people_upload, batch_type2_upload
+from utils.keymap import camera_ip2camera_id, abnormal_type_to_rule_id
 from processes.nodes.recognizer import AbnormalDetectionRecognizer
+from utils.socket_client import client
+from utils.time_utils import genera_stamp
+from processes.nodes.recognizer import RealTimeRecognizer
+from scenes import BaseEngineering, BaseScenesManage
+from processes.nodes.recorder import CameraReader
+from processes.nodes.fire_node import FlameDiffNode
+from processes.nodes.diff_node import FrameDiffNode
+from utils.image_base64 import array_to_file
 class AbnormalSchoolGateEngineering(BaseEngineering):
     '''实时检测学校出入大门异常情况
     '''
@@ -59,17 +60,15 @@ class AbnormalSchoolGateEngineering(BaseEngineering):
             'data': {}
         }}
         stamp = genera_stamp()
-        # TODO rule_id之后做一个映射
-        rule_id = 90002 if data.abnormal_type == 'stay_too_long' else 90001
+        rule_id = abnormal_type_to_rule_id[data.abnormal_type]
         result['data']['data']['camera_id'] = data.camera_key
         result['data']['data']['scenario_id'] = data.camera_key
         result['data']['stamp'] = stamp
         result['data']['rule_id'] = rule_id
-
         # 这里后面一个参数只是取这个list的长度作为生成paths的list长度，前者需要一个list。
         path = array_to_file([data.base64_data], [1])
         assert len(path) == 1
-        batch_type2_upload(path[0],data.camera_key,result['data']['stamp'])
+        batch_type2_upload(path[0], data.camera_key, result['data']['stamp'])
         client.send(json.dumps(result, ensure_ascii=False))
 
 
@@ -82,13 +81,21 @@ class AbnormalSchoolGateScene(BaseScenesManage):
         realtime_abnormal = AbnormalDetectionRecognizer()
         diff = FrameDiffNode(1)
         diff.init_node()
-        camera.init_node(
-            ['rtsp://admin:admin12345@192.168.0.52:554/Streaming/Channels/101'], ["1"], 10, "123")
+        flame = FlameDiffNode()
+        camera_ip = [
+            'rtsp://admin:admin12345@192.168.0.141:554/Streaming/Channels/101','rtsp://admin:sdkj123456@192.168.0.64:554/Streaming/Channels/101']
+        camera_id = [camera_ip2camera_id[ip] for ip in camera_ip]
+        # camera_id = ['1']
+        camera.init_node(camera_ip, camera_id, 100, "123")
         realtime_abnormal.init_node()
-
+        flame.init_node()
+        camera.run()
+        diff.run()
+        realtime_abnormal.run()
+        flame.run()
         self.nodes.append(camera)
         self.nodes.append(diff)
-        self.nodes.append(realtime_abnormal)
+        self.nodes.append([realtime_abnormal, flame])
         self.manage = AbnormalSchoolGateEngineering()
 
 
